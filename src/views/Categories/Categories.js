1419c800d7ff3a103dafb3c34191e18b6f3a32fa
nikhil gadakh <147181148+nikhilgadakhofficial@users.noreply.github.com>
2024-02-25 11:39:22 +0530

    Revert "categories changes"

--- src/views/Categories/Categories.js
+++ src/views/Categories/Categories.js
@@ -1,45 +1,9 @@
-import React from 'react';
-import './Categories.css';
-import Navbar from '../../Components/Navbar/Navbar';
+import React from 'react'
+import './Categories.css'
+function Categories() {
+  return (
+    <div>Categories</div>
+  )
+}
 
-const categories = [
-  {
-    name: 'Appetizers',
-    image: '/images/paneer-tikka.jpg'
-  },
-  {
-    name: 'Main Course',
-    image: '/images/maharashtraThali.jpeg',
-  },
-  {
-    name: 'Desserts',
-    image: '/images/kulfi.webp',
-  },
-  {
-    name: 'Drinks',
-    image: '/images/lassi.webp',
-  },
-];
-
-const CategoryCard = ({ name, image }) => (
-  <div className="category-card">
-    <div className="image-container">
-      <img src={image} alt={name} />
-    </div>
-    <h3>{name}</h3>
-  </div>
-);
-
-const Categories = () => (
-  <div className="App">
-    <h1>Categories of Food</h1>
-    <div className="categories-container">
-      {categories.map((category, index) => (
-        <CategoryCard key={index} name={category.name} image={category.image} />
-      ))}
-    </div>
-  </div>
-);
-
-
-export default Categories;
+export default Categories
